1e74f5f8784b68af22a3fa5e35a8c9ef712a7d73
PeryLoth <www.luisplata@gmail.com>
2025-06-08 18:25:35 -0500

    database

---
+++ database/migrations/2025_06_08_232511_alter_chat_id_bigint_in_telegram_chats.php
@@ -0,0 +1,21 @@
+<?php
+// database/migrations/xxxx_xx_xx_xxxxxx_alter_chat_id_bigint_in_telegram_chats.php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::table('telegram_chats', function (Blueprint $table) {
+            $table->unsignedBigInteger('chat_id')->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('telegram_chats', function (Blueprint $table) {
+            $table->integer('chat_id')->change();
+        });
+    }
+};
